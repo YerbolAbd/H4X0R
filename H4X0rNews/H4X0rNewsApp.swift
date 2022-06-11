635c9f242eebde492a6be2b2e4e287f39c5a0199
YerbolAbd <abdiyevyerbol@yahoo.com>
2022-06-11 11:20:39 +0600

    Initial Commit

---
+++ H4X0rNews/H4X0rNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0rNewsApp.swift
+//  H4X0rNews
+//
+//  Created by Ербол on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0rNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
